83f44b17baf4ba1205ff4c6481569a6669b8355f
xRayderx <josemarti1896@gmail.com>
2024-03-31 19:40:45 -0400

    added todo components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,42 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="contact">
+    <h1>Contactame</h1>
+    <ul>
+      <li v-for="icon in icons" :key="icon.name">
+        <span class="material-icons">{{ icon.name }}</span>
+        <div>{{ icon.text }}</div>
+      </li>
+    </ul>
   </div>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const icons = ref([
+  { name: 'alternate_email', text: 'by email'},
+  { name: 'local_phone', text: 'by phone'},
+  { name: 'local_post_office', text: 'by post'},
+  { name: 'local_fire_department', text: 'by smoke signal'},
+])
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+  .contact ul {
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+    max-width: 400px;
+    margin: 60px auto;
   }
-}
+  .contact li {
+    list-style-type: none;
+    background: white;
+    padding: 30px;
+    border-radius: 10px;
+    box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+    cursor: pointer;
+    line-height: 1.5em;
+  }
 </style>
